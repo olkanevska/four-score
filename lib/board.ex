dc004133b9868cd62e06049f341daee4f282cdb1
kevin larrabee <larrabeat@gmail.com>
2016-09-26 00:10:31 -0400

    clean up code

--- lib/board.ex
+++ lib/board.ex
@@ -6,37 +6,42 @@ defmodule Board do
   end
 
   def to_string(board) do
-    "-"
-    |> String.duplicate(board.columns)
+    # String.duplicate("__", board.columns) <> "_" |>
+    ""
     |> add_rows_to_string(board, board.rows)
+    |> add_columns_to_string(board.columns)
   end
 
-  defp add_rows_to_string(string, _board, row) when row == 0 do string end
-  defp add_rows_to_string(string, board, row) do
-    string <> "\n" <> row_to_string(board, row)
+  defp add_rows_to_string(string, board, row) when row > 0 do
+    string <> row_to_string(board, row)
     |> add_rows_to_string(board, row - 1)
   end
+  defp add_rows_to_string(string, _board, _row), do: string
 
   defp row_to_string(board, row) do
     "|" <> Enum.reduce(1..board.columns, "", fn (col, str) -> 
-      str <> board.grid[col][row]
-    end) <> "|"
+      str <> board.grid[col][row] <> "|"
+    end) <> "\n"
+  end
+
+  defp add_columns_to_string(string, cols) do
+    string <> " " <> Enum.join(1..cols, " ")
   end
 
   defp create_grid(columns, rows), do: create_columns(%{}, columns, rows)
 
-  defp create_columns(grid, column, _row) when column == 0 do grid end
-  defp create_columns(grid, column, row) do
+  defp create_columns(grid, column, row) when column > 0 do
     rows = create_rows(%{}, row)
     grid
     |> Map.put(column, rows)
     |> create_columns(column - 1, row)
   end
+  defp create_columns(grid, _column, _row), do: grid
 
-  defp create_rows(map, row) when row == 0 do map end
-  defp create_rows(map, row) do
+  defp create_rows(map, row) when row > 0 do
     map
-    |> Map.put(row, " ")
+    |> Map.put(row, "X")
     |> create_rows(row - 1)
   end
+  defp create_rows(map, _row), do: map
 end
--- lib/game_io.ex
+++ lib/game_io.ex
@@ -1,17 +1,10 @@
 defmodule Game.IO do
-  def get_string(string), do: string |> IO.gets |> String.trim
+  def get_string(prompt), do: prompt |> IO.gets |> String.trim
 
-  def get_num(string, min, max) do
-    get_string(string)
-    |> Integer.parse
-    |> check_num(min, max, string)
-  end
-
-  defp check_num({int, _}, min, max, _string) when int >= min and int <= max do
-    int
-  end
-
-  defp check_num(_, min, max, string) do
-    get_num(string, min, max)
+  def get_num(prompt, min, max) do
+    case prompt |> get_string |> Integer.parse do
+      {int, _} when int in min..max -> int
+      _ -> get_num(prompt, min, max)
+    end
   end
 end

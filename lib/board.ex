4495db2991d6f6ea3399f5f834a87141a34b0cd9
kevin larrabee <larrabeat@gmail.com>
2016-09-30 08:37:56 -0400

    add horizontal and vertical win checks

--- lib/board.ex
+++ lib/board.ex
@@ -1,27 +1,27 @@
 defmodule Board do
-  defstruct columns: nil, rows: nil, grid: nil
+  defstruct cols: nil, rows: nil, grid: nil, openings: nil
 
-  def new(columns \\ 7, rows \\ 7) do
-    grid = create_grid(%{}, columns, rows)
-    %Board{columns: columns, rows: rows, grid: grid}
+  def new(cols \\ 7, rows \\ 7) do
+    grid = create_grid(%{}, cols, rows)
+    %Board{grid: grid, cols: cols, rows: rows, openings: cols * rows}
   end
 
-  def find_open_cell(%Board{columns: cols} = board, col) when col in 1..cols do
+  def add_piece(%Board{grid: grid, openings: openings} = board, token, col, row) do
+    new_row_map = Map.put(grid[col], row, token)
+    new_grid = Map.put(grid, col, new_row_map)
+    %Board{board | grid: new_grid, openings: openings - 1}
+  end
+
+  def find_open_cell(%Board{cols: cols} = board, col) when col in 1..cols do
     Enum.find 1..board.rows, fn row ->
       board.grid[col][row] == " "
     end
   end
 
-  def add_piece(%Board{grid: grid} = board, token, col, row) do
-    new_row_map = Map.put(grid[col], row, token)
-    new_grid = Map.put(grid, col, new_row_map)
-    %Board{board | grid: new_grid}
-  end
-
   def to_string(board) do
-    "" # String.duplicate("__", board.columns) <> "_"
+    "" # String.duplicate("__", board.cols) <> "_"
     |> add_rows_to_string(board, board.rows)
-    |> add_columns_to_string(board.columns)
+    |> add_cols_to_string(board.cols)
   end
 
   defp add_rows_to_string(string, board, row) when row > 0 do
@@ -31,22 +31,22 @@ defmodule Board do
   defp add_rows_to_string(string, _board, _row), do: string
 
   defp row_to_string(board, row) do
-    Enum.reduce(1..board.columns, "", fn (col, str) ->
+    Enum.reduce(1..board.cols, "", fn (col, str) ->
       str <> "|" <> board.grid[col][row]
     end) <> "|\n"
   end
 
-  defp add_columns_to_string(string, cols) do
+  defp add_cols_to_string(string, cols) do
     string <> " " <> Enum.join(1..cols, " ")
   end
 
-  defp create_grid(grid, column, row) when column > 0 do
+  defp create_grid(grid, col, row) when col > 0 do
     rows = create_rows(%{}, row)
     grid
-    |> Map.put(column, rows)
-    |> create_grid(column - 1, row)
+    |> Map.put(col, rows)
+    |> create_grid(col - 1, row)
   end
-  defp create_grid(grid, _column, _row), do: grid
+  defp create_grid(grid, _col, _row), do: grid
 
   defp create_rows(map, row) when row > 0 do
     map
--- lib/game.ex
+++ lib/game.ex
@@ -3,7 +3,7 @@ defmodule Game do
 
   defstruct players: [], next_player: nil, board: nil, victor: nil
 
-  @tokens [first: "O", second: "X"]
+  @tokens [first: "X", second: "O"]
 
   def new, do: %Game{}
 
@@ -19,21 +19,23 @@ defmodule Game do
     board = case get_num("\nUse (1) standard or (2) custom board: ", 1, 2) do
       1 -> Board.new
       _ ->
-        columns = get_num("Number of columns (4-16): ", 4, 16)
-        rows = get_num("Number of rows (4-16): ", 4, 16)
-        Board.new(columns, rows)
+        cols = get_num("Number of columns (5-15): ", 5, 15)
+        rows = get_num("Number of rows (5-15): ", 5, 15)
+        Board.new(cols, rows)
     end
     %Game{game | board: board}
   end
 
   def loop(game) do
     IO.puts("\n#{game.board}\n")
-    {board, col, row} = choose_column(game)
 
-    case check_for_win(board, col, row) do
-      {:win, player} -> %Game{game | victor: player}
-      {:draw}        -> game
-      {:ok, board}   ->
+    player = Enum.at(game.players, game.next_player)
+    {board, col, row} = choose_col(game.board, player)
+
+    case check_for_win(board, col, row, player.token) do
+      :true -> %Game{game | victor: player}
+      :draw -> game
+      {:ok, board} ->
         loop(%Game{game | board: board, next_player: 1 - game.next_player})
     end
   end
@@ -41,33 +43,44 @@ defmodule Game do
   def finish(%Game{victor: victor}) when victor do nil end
   def finish(_game), do: nil
 
-  defp choose_column(game) do
-    player = Enum.at(game.players, game.next_player)
+  defp choose_col(board, player) do
+    col = get_num("#{player.name}, choose column: ", 1, board.cols)
 
-    col ="#{player.name}, choose column: "
-    |> get_num(1, game.board.columns)
-
-    case Board.find_open_cell(game.board, col) do
-      nil -> choose_column(game)
+    case Board.find_open_cell(board, col) do
+      nil -> choose_col(board, player)
       row ->
-        board = Board.add_piece(game.board, player.token, col, row)
-        print_board(board)
+        board = Board.add_piece(board, player.token, col, row)
         {board, col, row}
     end
   end
 
-  defp print_board(board) do
-    IO.puts("\n#{board}\n")
-  end
-
-  defp check_for_win(board, column, row) do
-    # check_vertical(board, column, row) ||
-    # check_first_diagonal(board, column, row) ||
-    # check_second_diagonal(board, column, row)
+  defp check_for_win(board, col, row, token) do
+    check_horizontal(board, col, row, token) ||
+    check_vertical(board, col, row, token) ||
+    check_diagonals(board, col, row) ||
     {:ok, board}
   end
 
-  defp check_vertical(_board, _column, row) when row < 4 do false end
-  defp check_vertical(%Board{grid: grid}, column, row) do
+  defp check_horizontal(%Board{cols: cols} = board, col, row, token) do
+    position_range(col, cols)
+    |> Enum.reduce("", fn (x, str) -> str <> board.grid[x][row] end)
+    |> String.contains?(String.duplicate(token, 4))
   end
+
+  defp check_vertical(_, _, row, _) when row < 4 do false end
+  defp check_vertical(%Board{rows: rows} = board, col, row, token) do
+    position_range(row, rows)
+    |> Enum.reduce("", fn (y, str) -> str <> board.grid[col][y] end)
+    |> String.contains?(String.duplicate(token, 4))
+  end
+
+  defp check_diagonals(_, _, row, _) when row < 4 do false end
+  defp check_diagonals(%Board{rows: rows} = board, col, row, token) do
+  end
+
+  defp position_range(pos, total), do: min_pos(pos)..max_pos(pos, total)
+  defp min_pos(pos) when pos > 3 do pos - 3 end
+  defp min_pos(pos) when pos <= 3 do 1 end
+  defp max_pos(pos, total) when pos < total - 2 do pos + 3 end
+  defp max_pos(pos, total) when pos >= total - 2 do total end
 end

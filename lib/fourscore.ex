0948708a82907737530f6117ca6d7d2f7388d91a
kevin larrabee <larrabeat@gmail.com>
2016-10-05 17:19:47 -0400

    clean up code

--- lib/fourscore.ex
+++ lib/fourscore.ex
@@ -2,10 +2,10 @@ defmodule Fourscore do
   def play do
     IO.puts("\nWelcome to Fourscore!\n")
 
-    Game.new()
-    |> Game.create_players()
-    |> Game.create_board()
-    |> Game.loop()
-    |> Game.finish()
+    Game.new
+    |> Game.create_players
+    |> Game.create_board
+    |> Game.loop
+    |> Game.finish
   end
 end
--- lib/game.ex
+++ lib/game.ex
@@ -11,54 +11,60 @@ defmodule Game do
   def create_players(%Game{} = game) do
     players = Enum.map(@tokens, fn {ordinal, token} ->
       "Enter #{ordinal} player name: "
-      |> UI.get_string()
+      |> UI.get_string
       |> Player.new(token)
     end)
     %Game{game | players: players, next_player: 0}
   end
 
   def create_board(%Game{} = game) do
-    board = case UI.get_num("\nUse (1) standard or (2) custom board: ", 1, 2) do
-      1 -> Board.new
+    case "Use (1) standard or (2) custom board: " |> UI.get_num(1, 2) do
+      1 ->
+        %Game{game | board: Board.new}
+      2 ->
+        cols = "Number of columns (5-15): " |> UI.get_num(5, 15)
+        rows = "Number of rows (5-15): "    |> UI.get_num(5, 15)
+        %Game{game | board: Board.new(cols, rows)}
       _ ->
-        cols = UI.get_num("Number of columns (5-15): ", 5, 15)
-        rows = UI.get_num("Number of rows (5-15): ", 5, 15)
-        Board.new(cols, rows)
+        create_board(game)
     end
-    %Game{game | board: board}
   end
 
-  def loop(%Game{} = game) do
-    IO.puts("\n#{game.board}\n")
+  def loop(%Game{board: %Board{} = board, next_player: next} = game) do
+    player = Enum.at(game.players, next)
 
-    player = Enum.at(game.players, game.next_player)
-    {board, col, row} = choose_col(game.board, player)
-
-    game = %Game{game | board: board}
+    print_board(board)
+    {new_board, col, row} = choose_col(board, player)
+    updated_game = %Game{game | board: new_board}
 
     cond do
-      Score.win?(board, col, row, player.token) ->
-        %Game{game | victor: player}
+      Score.win?(new_board, col, row, player.token) ->
+        %Game{updated_game | victor: player}
       Score.full?(board) ->
-        game
+        updated_game
       true ->
-        loop(%Game{game | next_player: 1 - game.next_player})
+        loop(%Game{updated_game | next_player: 1 - next})
     end
   end
 
   def finish(%Game{board: board, victor: victor}) do
-    IO.puts("\n#{board}\n")
+    print_board(board)
     case victor do
-      %Player{} -> IO.puts("#{victor.name} won!")
-      _ -> IO.puts("Draw.")
+      %Player{} ->
+        IO.puts("#{victor.name} won!")
+      _ ->
+        IO.puts("Draw.")
     end
   end
 
+  defp print_board(%Board{} = board), do: IO.puts("\n#{board}\n")
+
   defp choose_col(board, player) do
-    col = UI.get_num("#{player} - choose column: ", 1, board.cols)
+    col = "#{player} - choose column: " |> UI.get_num(1, board.cols)
 
     case Board.find_open_cell(board, col) do
-      nil -> choose_col(board, player)
+      nil ->
+        choose_col(board, player)
       row ->
         board = Board.add_piece(board, player.token, col, row)
         {board, col, row}

5ccec56fa94c78604aa11e6736af6dfbfb518dec
kevin larrabee <larrabeat@gmail.com>
2017-10-24 22:02:14 -0400

    Init repo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace fourscore_csharp {
+  class App {
+    public void Run() {
+      Console.WriteLine("Welcome to Four Score!");
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      App app = new App();
+      app.Run();
+    }
+  }
+}

9a907ea9979638705c59a2dc33748508f6b79345
kevin larrabee <larrabeat@gmail.com>
2017-10-24 21:44:18 -0400

    Miscellaneous cleanup

--- lib/four_score.rb
+++ lib/four_score.rb
@@ -1,6 +1,7 @@
-module FourScore
-end
-
-require_relative "./four_score/player.rb"
+require_relative "./four_score/app.rb"
 require_relative "./four_score/board.rb"
 require_relative "./four_score/game.rb"
+require_relative "./four_score/player.rb"
+
+module FourScore
+end
